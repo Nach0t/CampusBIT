ca2af72ca40368e201b4bdf4cd709cdf04cb927f
Ignacio Rehbein <78046088+Nach0t@users.noreply.github.com>
2024-11-23 23:29:13 -0300

    Update MenuScreen.dart

--- CampusBITs/lib/MenuScreen.dart
+++ CampusBITs/lib/MenuScreen.dart
@@ -45,144 +45,166 @@ class _MenuScreenState extends State<MenuScreen> {
       appBar: AppBar(
         title: Text(
           'Menú de Compra',
-          style: TextStyle(color: Colors.white),
+          style: TextStyle(
+            color: Colors.white,
+            fontFamily: 'Exo2',
+            fontWeight: FontWeight.bold,
+          ),
         ),
         centerTitle: true,
-        backgroundColor: Color(0xFF20B2AA), // Azul agua
+        backgroundColor: Color(0xFF87CEEB), // Celeste agua
         elevation: 5.0,
       ),
       body: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(
-            colors: [Color(0xFF66CDAA), Color(0xFF20B2AA)], // Azul agua degradado
+            colors: [Color(0xFFB0E0E6), Color(0xFF87CEEB)], // Degradado celeste agua
             begin: Alignment.topCenter,
             end: Alignment.bottomCenter,
           ),
         ),
         child: menuItems.isEmpty
             ? Center(
-          child: CircularProgressIndicator(),
-        )
-            : Column(
-          children: [
-            Expanded(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'Promoción Semanal:',
-                        style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                      SizedBox(height: 16),
-                      _buildPromoItem('Promo desayuno', 1800),
-                      _buildPromoItem('Promo almuerzo', 2500),
-                      SizedBox(height: 32),
-                      Text(
-                        'Almuerzos:',
-                        style: TextStyle(
-                          fontSize: 22,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white,
-                        ),
-                      ),
-                      SizedBox(height: 16),
-                      ListView.builder(
-                        physics: NeverScrollableScrollPhysics(),
-                        shrinkWrap: true,
-                        itemCount: menuItems.length,
-                        itemBuilder: (context, index) {
-                          final item = menuItems[index];
-                          return Card(
-                            color: Colors.white.withOpacity(0.1),
-                            elevation: 3,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(16.0),
-                            ),
-                            child: ListTile(
-                              title: Text(
-                                '${item['name']} - \$${item['price']}',
-                                style: TextStyle(
-                                  color: Colors.white,
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w600,
-                                ),
-                              ),
-                              trailing: ElevatedButton(
-                                onPressed: () {
-                                  _addToCart(item);
-                                },
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Color(0xFF20B2AA),
-                                  elevation: 3.0,
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(12.0),
-                                  ),
-                                ),
-                                child: Text('Añadir', style: TextStyle(color: Colors.white)),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ],
-                  ),
+                child: CircularProgressIndicator(
+                  color: Colors.white,
                 ),
-              ),
-            ),
-            Container(
-              color: Colors.grey[800],
-              padding: const EdgeInsets.all(16.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              )
+            : Column(
                 children: [
-                  Row(
-                    children: [
-                      Icon(Icons.shopping_cart, color: Colors.white),
-                      SizedBox(width: 8),
-                      Text(
-                        'Carrito (${getCartItemCount()})',
-                        style: TextStyle(color: Colors.white, fontSize: 18),
-                      ),
-                    ],
-                  ),
-                  ElevatedButton(
-                    onPressed: cartItems.isNotEmpty
-                        ? () {
-                      Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                          builder: (context) => CartScreen(
-                            cartItems: cartItems,
-                            username: widget.username,
-                          ),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: SingleChildScrollView(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              'Promoción Semanal:',
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Exo2',
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(height: 16),
+                            _buildPromoItem('Promo desayuno', 1800),
+                            _buildPromoItem('Promo almuerzo', 2500),
+                            SizedBox(height: 32),
+                            Text(
+                              'Almuerzos:',
+                              style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Exo2',
+                                color: Colors.white,
+                              ),
+                            ),
+                            SizedBox(height: 16),
+                            ListView.builder(
+                              physics: NeverScrollableScrollPhysics(),
+                              shrinkWrap: true,
+                              itemCount: menuItems.length,
+                              itemBuilder: (context, index) {
+                                final item = menuItems[index];
+                                return Card(
+                                  color: Colors.white.withOpacity(0.1),
+                                  elevation: 3,
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(16.0),
+                                  ),
+                                  child: ListTile(
+                                    title: Text(
+                                      '${item['name']} - \$${item['price']}',
+                                      style: TextStyle(
+                                        fontFamily: 'Exo2',
+                                        color: Colors.white,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w600,
+                                      ),
+                                    ),
+                                    trailing: ElevatedButton(
+                                      onPressed: () {
+                                        _addToCart(item);
+                                      },
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: Color(0xFF87CEEB),
+                                        elevation: 3.0,
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(12.0),
+                                        ),
+                                      ),
+                                      child: Text(
+                                        'Añadir',
+                                        style: TextStyle(
+                                          fontFamily: 'Exo2',
+                                          color: Colors.white,
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
                         ),
-                      );
-                    }
-                        : null,
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.white,
-                      elevation: 3.0,
-                      shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(12.0),
                       ),
                     ),
-                    child: Text(
-                      'Ir al Carrito',
-                      style: TextStyle(color: Color(0xFF20B2AA)),
+                  ),
+                  Container(
+                    color: Colors.grey[800],
+                    padding: const EdgeInsets.all(16.0),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.shopping_cart, color: Colors.white),
+                            SizedBox(width: 8),
+                            Text(
+                              'Carrito (${getCartItemCount()})',
+                              style: TextStyle(
+                                fontFamily: 'Exo2',
+                                color: Colors.white,
+                                fontSize: 18,
+                              ),
+                            ),
+                          ],
+                        ),
+                        ElevatedButton(
+                          onPressed: cartItems.isNotEmpty
+                              ? () {
+                                  Navigator.push(
+                                    context,
+                                    MaterialPageRoute(
+                                      builder: (context) => CartScreen(
+                                        cartItems: cartItems,
+                                        username: widget.username,
+                                      ),
+                                    ),
+                                  );
+                                }
+                              : null,
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.white,
+                            elevation: 3.0,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12.0),
+                            ),
+                          ),
+                          child: Text(
+                            'Ir al Carrito',
+                            style: TextStyle(
+                              fontFamily: 'Exo2',
+                              color: Color(0xFF87CEEB),
+                            ),
+                          ),
+                        ),
+                      ],
                     ),
                   ),
                 ],
               ),
-            ),
-          ],
-        ),
       ),
     );
   }
@@ -198,6 +220,7 @@ class _MenuScreenState extends State<MenuScreen> {
         title: Text(
           '$name - \$$price',
           style: TextStyle(
+            fontFamily: 'Exo2',
             color: Colors.white,
             fontSize: 18,
             fontWeight: FontWeight.w600,
@@ -208,13 +231,19 @@ class _MenuScreenState extends State<MenuScreen> {
             _addToCart({'name': name, 'price': price});
           },
           style: ElevatedButton.styleFrom(
-            backgroundColor: Color(0xFF20B2AA),
+            backgroundColor: Color(0xFF87CEEB),
             elevation: 3.0,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(12.0),
             ),
           ),
-          child: Text('Añadir', style: TextStyle(color: Colors.white)),
+          child: Text(
+            'Añadir',
+            style: TextStyle(
+              fontFamily: 'Exo2',
+              color: Colors.white,
+            ),
+          ),
         ),
       ),
     );
